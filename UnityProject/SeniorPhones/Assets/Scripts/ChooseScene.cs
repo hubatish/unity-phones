276b62672a6685d07f66cccab0c726ec5327563c
hubatish <zachariah.howell@gmail.com>
2015-10-01 15:00:02 -0400

    Added Scene Chooser - Testing separate client and server scenes

---
+++ UnityProject/SeniorPhones/Assets/Scripts/ChooseScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ChooseScene : NetworkBehaviour {
+
+    public Transform player;
+    public Transform client;
+
+	// Use this for initialization
+	void Start () {
+        if (!isServer)
+        {
+            if(isLocalPlayer)
+            {
+                Transform spawnedClient = (Transform)GameObject.Instantiate(client, transform.position, Quaternion.identity);
+                spawnedClient.parent = transform;
+                DontDestroyOnLoad(gameObject);
+                Application.LoadLevel(Application.loadedLevel + 1);
+            }
+        }
+        else
+        {
+            Transform spawnedPlayer = (Transform)GameObject.Instantiate(player, transform.position, Quaternion.identity);
+            spawnedPlayer.parent = transform;
+        }
+	}
+}
