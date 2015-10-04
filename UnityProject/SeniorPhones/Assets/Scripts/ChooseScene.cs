531f12b52ce1b5febcb7c72183f3f2d59f4cd6e9
jerbenne <jerbenne@gmail.com>
2015-10-03 20:15:09 -0400

    Added temporary display of IP address for convenience in host.

--- UnityProject/SeniorPhones/Assets/Scripts/ChooseScene.cs
+++ UnityProject/SeniorPhones/Assets/Scripts/ChooseScene.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using System.Net;
 
-public class ChooseScene : NetworkBehaviour {
+public class ChooseScene : NetworkBehaviour
+{
 
     public Transform player;
     public Transform client;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         if (!isServer)
         {
-            if(isLocalPlayer)
+            if (isLocalPlayer)
             {
                 Transform spawnedClient = (Transform)GameObject.Instantiate(client, transform.position, Quaternion.identity);
                 spawnedClient.parent = transform;
@@ -23,6 +26,11 @@ public class ChooseScene : NetworkBehaviour {
         {
             Transform spawnedPlayer = (Transform)GameObject.Instantiate(player, transform.position, Quaternion.identity);
             spawnedPlayer.parent = transform;
+
+            GameObject ipAddress = new GameObject();
+            ipAddress.AddComponent<GUIText>();
+            ipAddress.transform.position = new Vector3(0.5f, 0.9f, 0.0f);
+            ipAddress.GetComponent<GUIText>().text = "IP Address: " + Network.player.ipAddress;
         }
-	}
+    }
 }
