f9a757ae85bbf6303aba3d3924e9ff7fcda44721
hubatish <zachariah.howell@gmail.com>
2015-10-04 23:33:53 -0400

    Added Joystick mobile input alternative, sort of added holding d-pad. Made builds

---
+++ UnityProject/SeniorPhones/Assets/Scripts/Input/HoldButon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(EventTrigger))]
+public class HoldButon : ZBehaviour
+{
+
+    PlayerMovement playerMovement;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerMovement = ClientNetworkToolbox.Instance.GetNetworkMover();
+    }
+
+    [SerializeField]
+    protected Vector2 dir;
+
+    protected void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            playerMovement.CmdMoveInDirection(dir);
+        }
+    }
+}
---
+++ UnityProject/SeniorPhones/Assets/Scripts/Input/PhoneJoystick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Should be in a canvas
+/// </summary>
+public class PhoneJoystick : MonoBehaviour {
+
+    [SerializeField]
+    protected Canvas canvas;
+
+    [SerializeField]
+    protected Transform joystickImage;
+
+    PlayerMovement playerMovement;
+
+	// Use this for initialization
+	void Start () {
+        playerMovement = ClientNetworkToolbox.Instance.GetNetworkMover();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            //get mouse pos in UI coordinates (from http://answers.unity3d.com/questions/849117/46-ui-image-follow-mouse-position.html)
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out pos);
+            Vector3 dir = canvas.transform.TransformPoint(pos) - transform.position;
+            dir = dir.normalized;
+            joystickImage.position = transform.position + dir;
+            if (playerMovement != null)
+            {
+                playerMovement.CmdMoveInDirection(dir);
+            }
+            //Debug.Log("Dir: " + dir);
+        }
+        else
+        {
+            joystickImage.position = transform.position;
+            //enabled = false;
+        }
+    }
+}
