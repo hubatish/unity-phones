636cf03f0117ba692a9b553a68189531571c7a1a
Matt Walker <mjw355@drexel.edu>
2015-10-10 17:52:36 -0400

    CustomNetworkManager script created. StartMenu Scene created. Player object no longer goes through walls

---
+++ UnityProject/SeniorPhones/Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CustomNetworkManager : NetworkManager {
+
+	public void StartupGame()
+	{
+		SetPort();
+		NetworkManager.singleton.StartServer();
+		NetworkManager.singleton.ServerChangeScene("networkStart");
+	}
+
+	public void JoinGame()
+	{
+		SetIPAddress();
+		SetPort();
+		StartClient();
+	}
+
+	void SetIPAddress()
+	{
+		string ipAddress = GameObject.Find("InputIPAddress").transform.FindChild("Text").GetComponent<Text>().text;
+		networkAddress = ipAddress;
+	}
+
+	void SetPort()
+	{
+		networkPort = 7777;
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		if(level == 0)
+		{
+			SetupStartMenuSceneButtons();
+		}
+		else
+		{
+			//SetupClientSceneButtons();
+		}
+	}
+
+	void SetupStartMenuSceneButtons()
+	{
+		GameObject.Find("ButtonStartGame").GetComponent<Button>().onClick.RemoveAllListeners();
+		GameObject.Find("ButtonStartGame").GetComponent<Button>().onClick.AddListener(StartupGame);
+
+		GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.RemoveAllListeners();
+		GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.AddListener(JoinGame);
+	}
+
+	//void SetupClientSceneButtons()
+	//{
+	//	GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.RemoveAllListeners();
+	//	GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.AddListener(NetworkManager.singleton.StopServer);
+	//}
+}
---
+++ UnityProject/SeniorPhones/Assets/Scripts/SceneChooser.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Net;
+
+public class SceneChooser : NetworkBehaviour
+{
+
+    public Transform player;
+    public Transform client;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!isServer)
+        {
+            if (isLocalPlayer)
+            {
+                Transform spawnedClient = (Transform)GameObject.Instantiate(client, transform.position, Quaternion.identity);
+                spawnedClient.parent = transform;
+                DontDestroyOnLoad(gameObject);
+                Application.LoadLevel("clientScene");
+            }
+        }
+        else
+        {
+            Transform spawnedPlayer = (Transform)GameObject.Instantiate(player, transform.position, Quaternion.identity);
+            spawnedPlayer.parent = transform;
+
+            GameObject ipAddress = new GameObject();
+            ipAddress.AddComponent<GUIText>();
+            ipAddress.transform.position = new Vector3(0.5f, 0.9f, 0.0f);
+            ipAddress.GetComponent<GUIText>().text = "IP Address: " + Network.player.ipAddress;
+        }
+    }
+}
