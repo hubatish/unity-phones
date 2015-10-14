a437dec103561482874807ee4080c433890f7c8a
Vincent <vince.engard@gmail.com>
2015-10-13 21:03:32 -0400

    .\UnityProject

---
+++ UnityProject/SeniorPhones/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage : MonoBehaviour {
+    public float TimeBlinking = 0.5f;
+    float blinkTime;
+    Renderer myRenderer;
+	// Use this for initialization
+	void Start () {
+        myRenderer = GetComponentInChildren<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(blinkTime >= 0)
+        {
+            blinkTime -= Time.deltaTime;
+
+            myRenderer.enabled = !myRenderer.enabled;
+            
+            if(blinkTime <= 0)
+            {
+                myRenderer.enabled = true;
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("COLLISIONING");
+        if(coll.gameObject.tag == "Item")
+        {
+            blinkTime = TimeBlinking;
+            GetComponent<StateHandler>().StunPlayer();
+        }
+    }
+}
---
+++ UnityProject/SeniorPhones/Assets/Scripts/StateHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateHandler : MonoBehaviour {
+
+    private int CurrentState;
+    //0 = normal
+    //1 = stunned
+    //2 = recovery
+    //3 = attacking
+
+	// Use this for initialization
+	void Start () {
+        CurrentState = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Stun the player, Call Stunned Function
+    public void StunPlayer(){
+        StartCoroutine("Stunned");
+    }
+
+    //Change layers, can't move, and wait
+    private IEnumerator Stunned(){
+        CurrentState = 1;
+        GetComponent<PlayerMovement>().enabled = false;
+        yield return new WaitForSeconds(2);
+        //CHANGE LAYER,CALL RECOVERY
+        Debug.Log("I'm in Stunned!");
+        StartCoroutine("Recovering");
+    }
+
+    //Change to a different layer and wait, but can move now
+    public IEnumerator Recovering(){
+        CurrentState = 2;
+        GetComponent<PlayerMovement>().enabled = true;
+        yield return new WaitForSeconds(2);
+        Debug.Log("I'm in Recovery!");
+    }
+}
