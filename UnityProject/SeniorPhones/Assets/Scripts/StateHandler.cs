252d9108d3a023f31ccdc0259848e8836f630b10
Matt Walker <mjw355@drexel.edu>
2015-10-24 13:29:33 -0400

    State handler now has attack, cooldown, and severe stun states

--- UnityProject/SeniorPhones/Assets/Scripts/StateHandler.cs
+++ UnityProject/SeniorPhones/Assets/Scripts/StateHandler.cs
@@ -5,16 +5,22 @@ public class StateHandler : MonoBehaviour {
 
     private int currentState;
     private int normal = 0;
-    private int attacking = 1;
-    private int stun = 2;
-    private int recovery = 3;
+    private int attack = 1;
+    private int cooldown = 2;
+    private int stun = 3;
+    private int recovery = 4;
+    private int severeStun = 5;
 
     [SerializeField] private float prevMoveSpeed = 0;
+    private float stunDuration = 2f;
+    private float recoveryDuration = 2f;
+    private float cooldownDuration;
     private Server.PlayerMovement myMovement;
 
 	// Use this for initialization
 	void Start () {
         currentState = normal;
+        cooldownDuration = stunDuration;
         myMovement =GetComponent<Server.PlayerMovement>();
 	}
 	
@@ -24,28 +30,81 @@ public class StateHandler : MonoBehaviour {
 	}
 
     //Stun the player, Call Stunned Function
-    public void StunPlayer(){
-        if(currentState < 2)
+    public void StunPlayer()
+    {
+        if(currentState == normal || currentState == cooldown)
             StartCoroutine("Stunned");
+        else if(currentState == attack)
+            StartCoroutine("SeverelyStunned");
     }
 
-    //Change layers, can't move, and wait
-    private IEnumerator Stunned(){
+    public void PlayerAttack(float duration)
+    {
+        if(currentState == normal)
+            StartCoroutine("Attacking", duration);
+    }
+
+    private IEnumerator Attacking(float attackDuration)
+    {
+        currentState = attack;
+        Debug.Log("I'm Attacking!");
+        //Turn off attack button
+        yield return new WaitForSeconds(attackDuration);
+        if(currentState == attack)
+            StartCoroutine("AttackCooldown");
+    }
+
+
+    private IEnumerator AttackCooldown()
+    {
+        currentState = cooldown;
+        Debug.Log("I'm on Cooldown");
+        //Attack button stays off
+        yield return new WaitForSeconds(cooldownDuration);
+        if(currentState == cooldown)
+        {
+            currentState = normal;
+            //Turn on attack button
+        }
+
+    }
+
+    //can't move, and wait
+    private IEnumerator Stunned()
+    {
         currentState = stun;
         Debug.Log("I'm Stunned!");
-        prevMoveSpeed = (float) myMovement.GetMoveSpeed();
-        myMovement.SetMoveSpeed(0f);
-        yield return new WaitForSeconds(2);
+        StopPlayerMovement();
+        yield return new WaitForSeconds(stunDuration);
         //CHANGE LAYER,CALL RECOVERY
         StartCoroutine("Recovering");
     }
 
-    //Change to a different layer and wait, but can move now
-    public IEnumerator Recovering(){
+    //Change to a different layer so can't be hit and wait, but can move now
+    private IEnumerator Recovering()
+    {
         currentState = recovery;
         Debug.Log("I'm in Recovery!");
         myMovement.SetMoveSpeed(prevMoveSpeed);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(recoveryDuration);
         currentState = normal;
     }
+
+    private IEnumerator SeverelyStunned()
+    {
+        currentState = severeStun;
+        Debug.Log("I'm in Severely Stunned");
+        StopPlayerMovement();
+        yield return new WaitForSeconds(stunDuration * 1.5f);
+        StartCoroutine("Recovering");
+
+    }
+
+    private void StopPlayerMovement()
+    {
+        prevMoveSpeed = (float) myMovement.GetMoveSpeed();
+        myMovement.SetMoveSpeed(0f);
+    }
+
+
 }
